659524210f6f15b58e4f99ca9996970b5ff48051
qwe7002 <dev@nicoid.com>
2026-06-27 02:23:29 +0800

    refactor: replace RootView with ServerListView in app entry point and add server list functionality

--- ios/ServerCase/ServerCaseApp.swift
+++ ios/ServerCase/ServerCaseApp.swift
@@ -6,7 +6,7 @@ struct ServerCaseApp: App {
 
     var body: some Scene {
         WindowGroup {
-            RootView()
+            ServerListView()
                 .environmentObject(model)
                 .preferredColorScheme(.dark)
         }
